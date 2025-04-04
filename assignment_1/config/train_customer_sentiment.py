402b71f1b501f680b4ded3844ac3493277fbd4e8
ovguyo <ovguozdemir@gmail.com>
2025-04-04 21:31:39 +0300

    batch_size, lr, block_size changed in config

--- assignment_1/config/train_customer_sentiment.py
+++ assignment_1/config/train_customer_sentiment.py
@@ -15,16 +15,16 @@ wandb_run_name = 'nano-gpt'
 
 dataset = 'sentiment'
 gradient_accumulation_steps = 1
-batch_size = 4
-block_size = 256  # context of up to 256 previous characters
+batch_size = 64
+block_size = 1024  # context of up to 256 previous characters
 
 # baby GPT model :)
 n_layer = 6
 n_head = 6
 n_embd = 384
-dropout = 0.2
+dropout = 0.1
 
-learning_rate = 1e-3  # with baby networks can afford to go a bit higher
+learning_rate = 2e-3  # with baby networks can afford to go a bit higher
 max_iters = 2000
 lr_decay_iters = 2000  # make equal to max_iters usually
 min_lr = 1e-4  # learning_rate / 10 usually
